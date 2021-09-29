4aa8de261aa5f66456a6eb9d7c9d5c738550fa8f
pvnamm10 <nampvse151536@fpt.edu.vn>
2021-09-29 15:54:09 +0700

    update Test Green Alert

--- test/com/nampv/mathutil/core/AdvancedFactorialTest.java
+++ test/com/nampv/mathutil/core/AdvancedFactorialTest.java
@@ -39,7 +39,8 @@ public class AdvancedFactorialTest {
             {3,6},
             {4,24},
             {5,120},
-            {6,120}
+            {6,720},
+            {0,1}
         };
     }
     
